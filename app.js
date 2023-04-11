deb05de921760fc4ca011605ee7b841360304858
Emilie <em_bntd3112@yahoo.com>
2023-04-11 16:24:01 +0200

    home page - models + routes v1

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+require('./models/connection')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/trips');
 
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/trips', usersRouter);
 
 module.exports = app;
