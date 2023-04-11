22f3c2f728a916ef5a8ad9a8cd255d3521349441
Emilie <em_bntd3112@yahoo.com>
2023-04-11 16:21:34 +0200

    route trips v1 fonctionnelle

---
+++ routes/trips.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment')
+
+const fetch = require('node-fetch');
+const Trip = require('../models/trips');
+
+/* GET users listing. */
+router.get('/searchTrips', function(req, res) {
+  
+  let { departure, arrival, date } = req.body
+
+  if(!departure || departure === '' || !arrival || arrival === '' || !date || date === '') {
+    res.json({result: false, trip : 'Trip not found' })
+  } else {
+      departure  = departure[0].toUpperCase() + departure.slice(1).toLowerCase()
+      arrival  = arrival[0].toUpperCase() + arrival.slice(1).toLowerCase()
+      Trip.find({departure: departure, arrival: arrival}).then(data => {
+      
+        let arr = []
+    
+        if(data){
+    
+          for(let trip of data) {
+              let newDate = moment(trip.date).format('DD/MM/YYYY')
+              if(newDate === date) {
+                arr.push({departure : trip.departure, arrival: trip.arrival, date: moment(trip.date).format('HH:mm'), price: trip.price})
+              }
+            }
+            res.json({result: true, Trips: arr})
+          }
+
+      })
+  }
+
+
+  
+})
+
+module.exports = router;
